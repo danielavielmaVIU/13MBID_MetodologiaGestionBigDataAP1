ee8c457f25aa996152c92fcd39f71759f5f2ee00
Daniela <danielavielma@gmail.com>
2025-10-25 23:56:48 +0200

    Inicio de sesión práctica 4. Generación de test básicos con pytest.

--- test/test_data_quality.py
+++ test/test_data_quality.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pandera as pa
 from pandera import DataFrameSchema, Column, Check
 import pytest
+import datetime
+
 
 def datos():
     datos = pd.read_csv('data/raw/bank-additional-full.csv', sep=';')
@@ -37,3 +39,37 @@ def test_esquema():
     })
     
     esquema.validate(df, lazy=True)
+
+
+    def test_basico(datos_banco):
+        """Test inicial para verificar que el dataframe de datos_banco no esta vacío y contiene las columnas esperadas
+        
+        Agrs:
+            datos_banco (pd.DataFrame): DataFrame que contiene los datos del banco.
+        """
+        df = datos_banco
+
+        # Verficicar que el DataFRame no esta vacío
+        assert not df.empty, "El DataFram está vacío."
+
+        # Verficicar nulos
+        assert df.isnull().sim().sum() == 0, "El DataFram contiene valores nulos."
+
+        # Verificar cantidad de columnas
+        assert df.shape[1] == 21, f"El DataFrame deberia tener 21 coumnas, pero tiene {df.shape}"
+
+    if __name__ == "__name__":
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        nombre_archivo = f"docs/test_results/test_results_{timestamp}.txt"
+
+        try:
+            test_esquema(datos_banco())
+            test_basico(datos_banco())
+            print("Todos los tets pasaron exitosamente")
+            with open(nombre_archivo, "w") as f:
+                f.write("Todos los tets pasaron exitosamente \n")
+        except AssertionError as e:
+            print(f"Test fallido: {e}")
+            with open(nombre_archivo, "w") as f:
+                f.write("Test fallido: {e}\n")
