2f2c271a0a564d6385aa6da8aebaaf805785e7bd
Daniela <danielavielma@gmail.com>
2025-11-06 15:16:36 +0100

    Se modifica tratamiento de timestamp, se elimina el manejor del mismo

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,45 +1,46 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
 from pathlib import Path
 
 INPUT_CSV = 'data/raw/bank-additional-full.csv'
-OUTPUT_DIR = 'data/processed/'
+OUTPUT_CSV = 'data/processed/bank-processed.csv'
 
-def preprocess_data(input_path=INPUT_CSV, output_dir=OUTPUT_DIR):
 
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
+def preprocess_data(input_path=INPUT_CSV, output_path=OUTPUT_CSV):
+  
+    Path(output_path).parent.mkdir(parents=True, exist_ok=True)
 
+    # Load the dataset
     df = pd.read_csv(input_path, sep=';')
     
     # Adaptar nombres de columnas
     df.columns = df.columns.str.replace(".", "_")
 
-    # Transformar los valores  "Unknown" a NaN
-    df.replace ("unknown", np.nan , inplace=True)
+    # Transformar los valores 'unknown' en NaN
+    df.replace('unknown', np.nan, inplace=True)
 
     # Se agrega el campo contacted_before
     df['contacted_before'] = np.where(df['pdays'] == 999, 'no', 'yes')
 
-    # Se elimina la columna 'default' ya que tiene muchos valores desconocidos. Se elimina tambien pdays, dado la creación de variable contacted_before
+    # Se elimina la columna 'default' ya que tiene muchos valores desconocidos
     df.drop(columns=["default", "pdays"], inplace=True)
 
-    # Se hace un filtro para eliminar las filas que tiene valores nulos
+    # Se hace un filtro para eliminar las filas que tienen valores nulos
     df.dropna(inplace=True)
-
-    # Se hace filtro para elimnar las filas duplicadas
+    
+    # Se hace un filtro para eliminar las filas duplicadas
     df.drop_duplicates(inplace=True)
- 
-    # Mapea la columna objetivo 'y' a valores binarios
-    map = {'yes':1, 'no':0}
+
+    # Mapear la columna objetivo 'y' a valores binarios
+    map = {'yes': 1, 'no': 0}
     df['y'] = df['y'].map(map)
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_path = f"{output_dir}/bank-processed-full_{timestamp}.csv"
-
+    # Save the processed dataset
     df.to_csv(output_path, index=False)
 
+
     return df.shape
+
 
 if __name__ == "__main__":
     dimensiones = preprocess_data()
